561959f7f4160ff84a1f66ca34daed2c801831ad
Craig Schwarzwald <craig.schwarzwald@gmail.com>
2012-04-18 18:17:04 -0400

    Create setup() and tearDown() for Arquillian tests

--- src/test/java/com/example/domain/MyWebServiceTest.java
+++ src/test/java/com/example/domain/MyWebServiceTest.java
@@ -1,5 +1,8 @@
 package com.example.domain;
 
+import java.io.File;
+import java.util.ArrayList;
+
 import javax.inject.Inject;
 
 import org.jboss.arquillian.container.test.api.Deployment;
@@ -7,26 +10,153 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
 public class MyWebServiceTest {
-    @Inject private MyWebService mywebservice;
+	@Inject
+	private MyWebService mywebservice;
 
-    @Deployment public static WebArchive createDeployment() {
-        return ShrinkWrap.create(WebArchive.class, "test.war")
-                .addClasses(MyWebService.class)
-                .addClasses(User.class)
-                .addClasses(Player.class)
-                .addClasses(Link.class)
-                                .addAsResource("META-INF/persistence.xml")
-                                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
-    }
+	@Deployment
+	public static WebArchive createDeployment() {
+		return ShrinkWrap.create(WebArchive.class, "test.war")
+				.addClasses(MyWebService.class).addClasses(User.class)
+				.addClasses(Player.class)
+				.addClasses(Link.class)
+				.addClasses(TestUtils.class)
+				// Add the Gson Jar to the deployments with the following line:
+				.addAsLibrary(new File("lib/gson-2.1.jar"))
+				.addAsResource("META-INF/persistence.xml")
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
 
-    @Test
-    public void testIsDeployed() {
-        Assert.assertNotNull(mywebservice);
-    }
+	@Before
+	public void setup() throws Exception {
+		// Put all the sample data into our database
+
+		ArrayList<String> friendIDList = new ArrayList<String>();
+		friendIDList.add("\"newArray\"");
+		friendIDList.add("\"newArray\"");
+		TestUtils.createPlayerWithFriends(1000, "Ihave%20ZeroFriends",
+				friendIDList);
+
+		friendIDList.clear();
+		friendIDList.add("\"newArray\"");
+		friendIDList.add("\"67890\"");
+		friendIDList.add("\"newArray\"");
+		friendIDList.add("\"One Friend\"");
+		TestUtils.createPlayerWithFriends(1001, "Ihave%20OneFriends",
+				friendIDList);
+
+		friendIDList.clear();
+		friendIDList.add("\"newArray\"");
+		friendIDList.add("\"67890\"");
+		friendIDList.add("\"76543\"");
+		friendIDList.add("\"89012\"");
+		friendIDList.add("\"21098\"");
+		friendIDList.add("\"newArray\"");
+		friendIDList.add("\"One Friend\"");
+		friendIDList.add("\"Two Friend\"");
+		friendIDList.add("\"Three Friend\"");
+		friendIDList.add("\"Four Friend\"");
+		TestUtils.createPlayerWithFriends(1004, "Ihave%20FourFriends",
+				friendIDList);
+
+		friendIDList.clear();
+		friendIDList.add("\"newArray\"");
+		friendIDList.add("\"67890\"");
+		friendIDList.add("\"76543\"");
+		friendIDList.add("\"89012\"");
+		friendIDList.add("\"21098\"");
+		friendIDList.add("\"91234\"");
+		friendIDList.add("\"newArray\"");
+		friendIDList.add("\"One Friend\"");
+		friendIDList.add("\"Two Friend\"");
+		friendIDList.add("\"Three Friend\"");
+		friendIDList.add("\"Four Friend\"");
+		friendIDList.add("\"Five Friend\"");
+		TestUtils.createPlayerWithFriends(1005, "Ihave%20FiveFriends",
+				friendIDList);
+
+		friendIDList.clear();
+		friendIDList.add("\"newArray\"");
+		friendIDList.add("\"67890\"");
+		friendIDList.add("\"76543\"");
+		friendIDList.add("\"89012\"");
+		friendIDList.add("\"21098\"");
+		friendIDList.add("\"91234\"");
+		friendIDList.add("\"77441\"");
+		friendIDList.add("\"88552\"");
+		friendIDList.add("\"99663\"");
+		friendIDList.add("\"11223\"");
+		friendIDList.add("\"44556\"");
+		friendIDList.add("\"newArray\"");
+		friendIDList.add("\"One Friend\"");
+		friendIDList.add("\"Two Friend\"");
+		friendIDList.add("\"Three Friend\"");
+		friendIDList.add("\"Four Friend\"");
+		friendIDList.add("\"Five Friend\"");
+		friendIDList.add("\"Six Friend\"");
+		friendIDList.add("\"Seven Friend\"");
+		friendIDList.add("\"Eight Friend\"");
+		friendIDList.add("\"Nine Friend\"");
+		friendIDList.add("\"Ten Friend\"");
+		TestUtils.createPlayerWithFriends(1010, "Ihave%20TenFriends",
+				friendIDList);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		// Clear all the sample data out of our database now that we're done
+		// running all the tests.
+
+		String response = TestUtils.removePlayer(1000);
+		Assert.assertTrue(response
+				.equals("Player removed with FacbookID: 1000"));
+		response = TestUtils.removePlayer(1001);
+		Assert.assertTrue(response
+				.equals("Player removed with FacbookID: 1001"));
+		response = TestUtils.removePlayer(1004);
+		Assert.assertTrue(response
+				.equals("Player removed with FacbookID: 1004"));
+		response = TestUtils.removePlayer(1005);
+		Assert.assertTrue(response
+				.equals("Player removed with FacbookID: 1005"));
+		response = TestUtils.removePlayer(1010);
+		Assert.assertTrue(response
+				.equals("Player removed with FacbookID: 1010"));
+	}
+
+	@Test
+	public void testSetupData() {
+		// Retrieve the sample data from our database and make sure it's what we
+		// expect
+
+		Player playerWith0Friends = TestUtils.getPlayer(1000);
+		Player playerWith1Friends = TestUtils.getPlayer(1001);
+		Player playerWith4Friends = TestUtils.getPlayer(1004);
+		Player playerWith5Friends = TestUtils.getPlayer(1005);
+		Player playerWith10Friends = TestUtils.getPlayer(1010);
+
+		Assert.assertTrue(playerWith0Friends.getFriendList().size() == 0);
+		Assert.assertTrue(playerWith1Friends.getFriendList().size() == 1);
+		Assert.assertTrue(playerWith4Friends.getFriendList().size() == 4);
+		Assert.assertTrue(playerWith5Friends.getFriendList().size() == 5);
+		Assert.assertTrue(playerWith10Friends.getFriendList().size() == 10);
+
+		Assert.assertTrue(playerWith10Friends.getFriendList().get(0) == 67890);
+		User twoFriend = TestUtils.getUser(playerWith5Friends.getFriendList()
+				.get(1));
+		Assert.assertTrue(twoFriend.getName().equals("Two Friend"));
+	}
+
+	@Test
+	public void testIsDeployed() {
+		Assert.assertNotNull(mywebservice);
+	}
+
 }
---
+++ src/test/java/com/example/domain/TestUtils.java
@@ -0,0 +1,139 @@
+package com.example.domain;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+
+public class TestUtils {
+
+	private static String appName = "FBTutorialDemo";
+
+	public static String doGET(String URLstring) {
+		try {
+			String returnVal = "";
+
+			URL url = new URL(URLstring);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+
+			if (conn.getResponseCode() != 200) {
+				returnVal = "Failed : HTTP error code : "
+						+ conn.getResponseCode();
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					(conn.getInputStream())));
+
+			String output;
+			while ((output = br.readLine()) != null) {
+				returnVal += output;
+			}
+
+			conn.disconnect();
+			return returnVal;
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static String doPOST(String URLstring, String JSONInput) {
+		try {
+			String returnVal = "";
+
+			URL url = new URL(URLstring);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Type", "application/json");
+
+			OutputStreamWriter wr = new OutputStreamWriter(
+					conn.getOutputStream());
+			wr.write(JSONInput);
+			wr.flush();
+
+			// Get the response
+			BufferedReader rd = new BufferedReader(new InputStreamReader(
+					conn.getInputStream()));
+			String line;
+			while ((line = rd.readLine()) != null) {
+				returnVal += line;
+			}
+			wr.close();
+			rd.close();
+			return returnVal;
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			return "printStackTrace error: " + e;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "IOException error: " + e;
+		}
+	}
+
+	public static Player createPlayerWithFriends(long facebookID, String name,
+			ArrayList<String> friendIDList) {
+		String targetURL = "http://localhost:8080/" + appName
+				+ "/rest/webService/UserRequest/" + facebookID + "/" + name;
+		String JSONInput = "";
+		String response = doPOST(targetURL, JSONInput);
+
+		targetURL = "http://localhost:8080/" + appName
+				+ "/rest/webService/PlayerRequest/" + facebookID + "/" + name;
+		JSONInput = friendIDList.toString();
+		response = doPOST(targetURL, JSONInput);
+
+		targetURL = "http://localhost:8080/" + appName
+				+ "/rest/webService/Player/" + facebookID;
+		response = doGET(targetURL);
+
+		Gson gsonPlayer = new Gson();
+		Player responsePlayer = gsonPlayer.fromJson(response, Player.class);
+
+		return responsePlayer;
+	}
+
+	public static User getUser(long facebookID) {
+		String targetURL = "http://localhost:8080/" + appName
+				+ "/rest/webService/User/" + facebookID;
+		String response = doGET(targetURL);
+
+		Gson gsonUser = new Gson();
+		User responseUser = gsonUser.fromJson(response, User.class);
+
+		return responseUser;
+	}
+
+	public static Player getPlayer(long facebookID) {
+		String targetURL = "http://localhost:8080/" + appName
+				+ "/rest/webService/Player/" + facebookID;
+		String response = doGET(targetURL);
+
+		Gson gsonPlayer = new Gson();
+		Player responsePlayer = gsonPlayer.fromJson(response, Player.class);
+
+		return responsePlayer;
+	}
+
+	public static String removePlayer(long facebookID) {
+		String targetURL = "http://localhost:8080/" + appName
+				+ "/rest/webService/PlayerRemovalRequest/" + facebookID;
+		String JSONInput = "";
+		String response = doPOST(targetURL, JSONInput);
+
+		return response;
+	}
+}
